f18bfb8c847e9b1e90dee8deddf0b2ba7d9b8fe6
keeykeey <sh1n_fuj1to@icloud.com>
2022-09-17 15:06:17 +0900

    murmurhash header

---
+++ src/core/bnx_murmurhash.h
@@ -0,0 +1,19 @@
+
+/*
+ *
+ *
+ */
+
+
+#ifndef _BNX_MURMURHASH_H_INCLUDED_
+#define _BNX_MURMURHASH_H_INCLUDED_
+
+
+#include "bnx_config.h"
+#include "bnx_core.g"
+
+
+uint32_t bnx_murmur_hash2(u_char *data, size_t len);
+
+
+#endif /* _BNX_MURMURHASH_H_INCLUDED_ */
