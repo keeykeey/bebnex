0f4f072d4eef741c4e29e2ace8d008dd28107dd4
shinfujito <sh1n_fuj1to@icloud.com>
2024-02-19 01:38:14 +0900

    add bebnex minimam functionality

--- src/core/bebnex.c
+++ src/core/bebnex.c
@@ -1,6 +1,16 @@
 #include "./core.h"
+#include "./config.h"
 
 int main(void)
 {
     printf("hello bebnex!\n");
+    int port = 8080; // TODO: read from setting file
+    int max_con = 10; // TODO: read from setting file
+    
+    bnx_socket_t sock = bnx_create_socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    sockaddr_in_t sin = bnx_create_sockaddr_in(AF_INET, INADDR_ANY, port);
+    bnx_bind_socket(sock.fd, sin);
+    bnx_listen_socket(sock.fd, max_con);
+    bnx_launch(sock, sin);
+    close(sock.fd);
 }
--- src/event/bnx_socket.h
+++ src/event/bnx_socket.h
@@ -2,6 +2,7 @@
 #define BNX_SOCKET_H
 
 #include "../core/core.h"
+#define BUF_LEN 1024
 
 typedef struct bnx_socket_s {
     bnx_int_t domain;
@@ -12,5 +13,9 @@ typedef struct bnx_socket_s {
 } bnx_socket_t;
 
 bnx_socket_t bnx_create_socket(int domain, int type, int protocol);
+sockaddr_in_t bnx_create_sockaddr_in(int fd, in_addr_t addr, int port);
+int bnx_bind_socket(int fc, sockaddr_in_t sin);
+int bnx_listen_socket(int fd, int max_con);
+int bnx_launch(bnx_socket_t sock, sockaddr_in_t sin);
 
 #endif /** BNX_SOCKET_H */
