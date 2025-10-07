8635db1e1664e29665539534e2b6d925269f6c34
shinfujito <sh1n_fuj1to@icloud.com>
2025-10-08 00:37:45 +0900

    implemented logging to read config module

--- src/core/bnx_log.h
+++ src/core/bnx_log.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <stdio.h>
 
 #ifdef __cplusplus
 extern "C" {
--- src/main.c
+++ src/main.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include "core/bnx_log.h"
 #include "core/bnx_conf_file.h"
